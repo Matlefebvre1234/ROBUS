d2784aa565b49b4e4dec277815444038a758c284
Daniel Bilodeau <danyboy20009@gmail.com>
2020-10-06 13:06:08 -0400

    Update main.cpp

--- ROBUS_DARTH_GENIUS/src/main.cpp
+++ ROBUS_DARTH_GENIUS/src/main.cpp
@@ -13,15 +13,16 @@ void setup()
     BoardInit();
 }
 
-void LigneDroitePID (int vitesse, float vit_motg, float vit_motd, int dist_reel_tot, int nbcycle)
+void LigneDroitePID (void)
 {
     int dist_tot = vitesse * nbcycle;
-    float kp = 0.0001;
-    float ki = 0.00002;
+    float kp = 0.00001;
+    float ki = 0.000002;
+    double comp = 0;
 
     
     int vitesselu_g = ENCODER_ReadReset (LEFT);
-    int vitesselu_d = ENCODER_ReadReset (RIGHT); 
+    int vitesselu_d = ENCODER_ReadReset (RIGHT);
 
     int erreur_g = vitesse - vitesselu_g;
     int erreur_d = vitesse - vitesselu_d;
@@ -29,19 +30,19 @@ void LigneDroitePID (int vitesse, float vit_motg, float vit_motd, int dist_reel_
     int erreur_vit = erreur_g - erreur_d;
     int erreur_dist = (dist_tot) - dist_reel_tot;
 
-    float comp = (erreur_vit * kp) + (erreur_dist * ki);
+    comp = ((double)erreur_vit * kp) + ((double)erreur_dist * ki);
 
     vit_motg = vit_motg + (comp / 2);
     vit_motd = vit_motd - (comp / 2);
 
     MOTOR_SetSpeed (LEFT, vit_motg);
     MOTOR_SetSpeed (RIGHT, vit_motd);
-    dist_reel_tot = dist_reel_tot + ((vitesselu_g + vitesselu_d)/ 2);
+    dist_reel_tot = dist_reel_tot + (vitesse + ((vitesselu_g - vitesselu_d)/ 2));
 
 }
 void loop()
 {
-    LigneDroitePID( vitesse, vit_motg, vit_motd, dist_reel_tot, nbcycle);
+    LigneDroitePID();
     nbcycle = nbcycle + 1;
     delay (delais);
 }
