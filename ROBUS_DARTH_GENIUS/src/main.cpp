c2689eb2d9ef832e90b6cb15e1d2c81e6a47dfb8
AWhiteSheep <43411943+AWhiteSheep@users.noreply.github.com>
2020-10-14 17:39:50 -0400

    Pret pour le combat!
    changement du delai de mort au ligne droite PID2
    ajout de changement de la valeur de lacceleration pour une distance et ajustement de la vitesse selon lEncodeur de la roue

--- ROBUS_DARTH_GENIUS/src/main.cpp
+++ ROBUS_DARTH_GENIUS/src/main.cpp
@@ -6,8 +6,8 @@
 #define TURN 1
 int g_direction = 1;
 int vitesse = 300;
-float g_vit_motg_Origin = 0.55;
-float g_vit_motd_Origin = 0.55;
+float g_vit_motg_Origin = 0.70;
+float g_vit_motd_Origin = 0.70;
 float g_vit_motd = g_vit_motd_Origin;
 float g_vit_motg = g_vit_motg_Origin;
 float derniereValeurLuGPulse = 0;
@@ -96,7 +96,7 @@ void LigneDroitePID2()
     g_vit_motd = g_vit_motd - direction_droite*(comp_d/2);
     g_vit_motg = g_vit_motg - direction_gauche*(comp_g/2);
     // FIN NOUVEAU
-    delay(10);
+    delay(50);
     MOTOR_SetSpeed(RIGHT,g_vit_motd);
     MOTOR_SetSpeed(LEFT,g_vit_motg);
 }
@@ -104,32 +104,38 @@ void LigneDroitePID2()
 void Avancer(int pulse)
 {
     int deceleration = pulse - (pulse * 0.1);
+    float acceleration_v;
+    float n_pulse_bar = 4200;
     bool accel = true;
+
 
     g_vit_motd = 0.22;
     g_vit_motg = 0.22;
+    acceleration_v = (g_vit_motg_Origin - g_vit_motd)/n_pulse_bar;
     Serial.println("DÉBUT: " + String(pulse));
+    LigneDroitePID2();
+    delay(250);
     while(dist_reel_totG < pulse - derniereValeurLuGPulse)
     {
         // acceleration du début
         if (g_vit_motg < g_vit_motg_Origin && accel == true)
-            g_vit_motg += 0.03;
+            g_vit_motg += acceleration_v*gt_derniere_lu_G_D[LEFT];
         else
         {
             accel = false;
         }
         if (g_vit_motd < g_vit_motd_Origin && accel == true)
-            g_vit_motd += 0.03;
+            g_vit_motd += acceleration_v*gt_derniere_lu_G_D[RIGHT];
         else
         {
             accel = false;
         }
         
         //decceleration
-        if(pulse - dist_reel_totG < 3200 & g_vit_motd > 0.1 && g_vit_motd > 0.1)
+        if(pulse - dist_reel_totG < n_pulse_bar & g_vit_motd > 0.1 && g_vit_motd > 0.1)
         {
-            g_vit_motg -= 0.028;
-            g_vit_motd -= 0.028;
+            g_vit_motg -= acceleration_v*gt_derniere_lu_G_D[LEFT];
+            g_vit_motd -= acceleration_v*gt_derniere_lu_G_D[RIGHT];
             Serial.println("distance reel total G: " + String(dist_reel_totG));
             LigneDroitePID2();
         }
@@ -190,6 +196,7 @@ void Virage_2roue(float angle)
     Serial.print("pulse_afaire/2: ");
     Serial.println(pulse_distribution);
 
+    LigneDroitePID2();
     while(abs(ENCODER_Read(roue_maitre)) <= pulse_distribution - abs(gt_dist_total_reelG_D[roue_maitre]))
     {
         LigneDroitePID2();
@@ -226,8 +233,8 @@ void loop()
         {90, FRONT},
         {90, TURN},
         {90, FRONT},
-        {45, TURN},
-        {180, FRONT},
+        {40, TURN},
+        {190, FRONT},
         {-90, TURN},
         {64, FRONT},
         {45, TURN},
